56a06fa53b3c691d5f499c514f50c8cce4db93ee
treipatru <andrei@planet34.org>
2018-04-29 17:34:29 +0200

    Updated data handling * Added function to retrieve a random key from redis and return it as a dictionary * Updated view data export

--- parnaie/app.py
+++ parnaie/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, send_from_directory
 import redis
+import json
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -16,14 +17,28 @@ def create_app():
     app.config.from_pyfile('settings.py', silent=True)
     r = redis.StrictRedis(host='parnaie_redis_1', port=6379)
 
+    def getItem ():
+        key = r.randomkey()
+        item = { 
+            "word": r.hget(key, 'word'),
+            "props": [
+                r.hget(key, 'prop0'), r.hget(key, 'prop1'),
+                r.hget(key, 'prop2'), r.hget(key, 'prop3')
+            ],
+            "defs": [
+                r.hget(key, 'def0'), r.hget(key, 'def1'), r.hget(key, 'def2'),
+                r.hget(key, 'def3'), r.hget(key, 'def4'), r.hget(key, 'def5'),
+                r.hget(key, 'def6')
+            ]
+        }
+        return item;
+
     @app.route('/')
     def index():
-        word = r.randomkey()
-        definition = r.get(word)
+        key = r.randomkey()
         return render_template(
         'index.html',
-        word=word,
-        definition=definition
+        wordObj = json.dumps(getItem())
         )
 
     @app.route('/despre')
